3fb1b7249c9b05e412f12a65d718526ca822ce2e
Hunt Redmine <hunt@red-mine.com>
2023-04-06 13:03:22 +0800

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # resources :stocks
   # resources :staves
-  root  "stocks#index"
-  get   "/stocks",          to: "stocks#index"
+
   get   "/:area",           to: "stocks#index"
   get   "/stocks/:stock",   to: "stocks#show"
+
+  root  "stocks#index"
 end
