b54f180183b71b4bb4b5b1730fda7d0a92a34bb5
Hunt Redmine <hunt@red-mine.com>
2023-04-06 13:23:30 +0800

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # resources :stocks
   # resources :staves
 
-  get   "/:area",           to: "stocks#index"
-  get   "/stocks/:stock",   to: "stocks#show"
+  get   "/:area",         to: "stocks#index"
+  get   "/stocks/:stock", to: "stocks#show"
 
   root  "stocks#index"
 end
